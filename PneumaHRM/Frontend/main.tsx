c24d6e5c75b69d4e52e38216943a84e52b450c1f
weitalu <weitalu@pneumasoftware.com>
2019-04-18 16:50:44 +0800

    add route example

--- PneumaHRM/Frontend/main.tsx
+++ PneumaHRM/Frontend/main.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import Dashboard from './dashboard/Dashboard'
 
-ReactDOM.render(<Dashboard />, document.querySelector('#root'));
+
+render(<BrowserRouter>
+    <div>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/topics">Topics</Link>
+            </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/about" component={Dashboard} />
+        <Route path="/topics" component={Dashboard} />
+    </div>
+</BrowserRouter>, document.querySelector('#root'));
