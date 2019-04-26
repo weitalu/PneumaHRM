1739af212877523235ad97aabf9b7a247568a3bb
weitalu <weitalu@pneumasoftware.com>
2019-04-26 13:27:54 +0800

    prepare dashboard

--- PneumaHRM/Frontend/dashboard/index.tsx
+++ PneumaHRM/Frontend/dashboard/index.tsx
@@ -1,47 +1,90 @@
 import React from 'react';
 
-import gql from 'graphql-tag';
+import DASHBOARD_DATA_QUERY from './query';
 import { Query } from 'react-apollo';
 
-const GET_REPOSITORIES_OF_ORGANIZATION = gql`
-{
-    holidays(where: [{path: "value", comparison: greaterThanOrEqual, value: ["2019-01-01"]}]) {
-      value
-      description
-    }
-    leaveRequests {
-      from
-      to
-      owner
-    }
-    leaveRequestsConnection {
-      totalCount
-      pageInfo {
-        endCursor
-        hasNextPage
-      }
-      items {
-        id
-        name
-        state
-        type
-        from
-        to
-      }
+import FullCalendar from '@fullcalendar/react'
+import { EventInput } from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction' // needed for dayClick
+
+
+var a = () => <Query query={DASHBOARD_DATA_QUERY} children={({ data, loading }) => <>{JSON.stringify(data)}</>}>
+</Query>
+
+
+
+import './main.scss'
+
+interface DemoAppState {
+  calendarWeekends: boolean
+  calendarEvents: EventInput[]
+}
+
+export default class DemoApp extends React.Component<{}, DemoAppState> {
+
+  calendarComponentRef = React.createRef<FullCalendar>()
+
+  constructor(props: {}) {
+    super(props)
+
+    this.state = {
+      calendarWeekends: true,
+      calendarEvents: [ // initial event data
+        { title: 'Event Now', start: new Date() }
+      ]
     }
   }
-  
-`;
 
+  render() {
+    return (
+      <div className='demo-app'>
+        <div className='demo-app-top'>
+          <button onClick={ this.toggleWeekends }>toggle weekends</button>&nbsp;
+          <button onClick={ this.gotoPast }>go to a date in the past</button>&nbsp;
+          (also, click a date/time to add an event)
+        </div>
+        <div className='demo-app-calendar'>
+          <FullCalendar
+            defaultView="dayGridMonth"
+            header={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+            }}
+            plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+            ref={ this.calendarComponentRef }
+            weekends={ this.state.calendarWeekends }
+            events={ this.state.calendarEvents }
+            dateClick={ this.handleDateClick }
+            />
+        </div>
+      </div>
+    )
+  }
 
-export default () => <Query query={GET_REPOSITORIES_OF_ORGANIZATION}>
-    {({ data: { holidays }, loading }) => {
-        if (loading || !holidays) {
-            return <div>Loading ...</div>;
-        }
+  toggleWeekends = () => {
+    this.setState({ // update a property
+      calendarWeekends: !this.state.calendarWeekends
+    })
+  }
 
-        return (
-            <>{JSON.stringify(holidays)}</>
-        );
-    }}
-</Query>
+  gotoPast = () => {
+    let calendarApi = this.calendarComponentRef.current!.getApi()
+    calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
+  }
+
+  handleDateClick = (arg: any) => {
+    if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
+      this.setState({  // add new event data
+        calendarEvents: this.state.calendarEvents.concat({ // creates a new array
+          title: 'New Event',
+          start: arg.date,
+          allDay: arg.allDay
+        })
+      })
+    }
+  }
+
+}
---
+++ PneumaHRM/Frontend/dashboard/query.ts
@@ -0,0 +1,31 @@
+import { gql } from 'apollo-boost'
+
+export default gql`
+{
+    holidays(where: [{path: "value", comparison: greaterThanOrEqual, value: ["2019-01-01"]}]) {
+      value
+      description
+    }
+    leaveRequests {
+      from
+      to
+      owner
+    }
+    leaveRequestsConnection {
+      totalCount
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+      items {
+        id
+        name
+        state
+        type
+        from
+        to
+      }
+    }
+  }
+  
+`
