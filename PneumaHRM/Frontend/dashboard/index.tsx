38793c2f71b343684cc0c3c02ad684e266cf5a38
weitalu <weitalu@pneumasoftware.com>
2019-04-26 17:11:18 +0800

    add calendar

--- PneumaHRM/Frontend/dashboard/index.tsx
+++ PneumaHRM/Frontend/dashboard/index.tsx
@@ -10,10 +10,17 @@ import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction' // needed for dayClick
 
 
-var a = () => <Query query={DASHBOARD_DATA_QUERY} children={({ data, loading }) => <>{JSON.stringify(data)}</>}>
+export default () => <Query query={DASHBOARD_DATA_QUERY} children>
+ {({ data: { holidays }, loading }) => {
+      if (loading || !holidays) {
+        return <div>Loading ...</div>;
+      }
+
+      return (
+        <DemoApp holidays={holidays.map(day=>({title: day.name , start:day.value}))}/>
+      );
+    }}
 </Query>
-
-
 
 import './main.scss'
 
@@ -22,7 +29,7 @@ interface DemoAppState {
   calendarEvents: EventInput[]
 }
 
-export default class DemoApp extends React.Component<{}, DemoAppState> {
+class DemoApp extends React.Component<{holidays: EventInput[]}, DemoAppState> {
 
   calendarComponentRef = React.createRef<FullCalendar>()
 
@@ -31,20 +38,13 @@ export default class DemoApp extends React.Component<{}, DemoAppState> {
 
     this.state = {
       calendarWeekends: true,
-      calendarEvents: [ // initial event data
-        { title: 'Event Now', start: new Date() }
-      ]
+      calendarEvents: [ ]
     }
   }
 
   render() {
     return (
       <div className='demo-app'>
-        <div className='demo-app-top'>
-          <button onClick={ this.toggleWeekends }>toggle weekends</button>&nbsp;
-          <button onClick={ this.gotoPast }>go to a date in the past</button>&nbsp;
-          (also, click a date/time to add an event)
-        </div>
         <div className='demo-app-calendar'>
           <FullCalendar
             defaultView="dayGridMonth"
@@ -56,35 +56,16 @@ export default class DemoApp extends React.Component<{}, DemoAppState> {
             plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
             ref={ this.calendarComponentRef }
             weekends={ this.state.calendarWeekends }
-            events={ this.state.calendarEvents }
-            dateClick={ this.handleDateClick }
+            events={ this.props.holidays }
+            businessHours={this.props.holidays}
+            dateClick={ (arg)=>this.handleDateClick(arg) }
             />
         </div>
       </div>
     )
   }
 
-  toggleWeekends = () => {
-    this.setState({ // update a property
-      calendarWeekends: !this.state.calendarWeekends
-    })
-  }
-
-  gotoPast = () => {
-    let calendarApi = this.calendarComponentRef.current!.getApi()
-    calendarApi.gotoDate('2000-01-01') // call a method on the Calendar object
-  }
-
-  handleDateClick = (arg: any) => {
-    if (confirm('Would you like to add an event to ' + arg.dateStr + ' ?')) {
-      this.setState({  // add new event data
-        calendarEvents: this.state.calendarEvents.concat({ // creates a new array
-          title: 'New Event',
-          start: arg.date,
-          allDay: arg.allDay
-        })
-      })
-    }
+  handleDateClick = (arg) => {
   }
 
 }
--- PneumaHRM/Frontend/dashboard/query.ts
+++ PneumaHRM/Frontend/dashboard/query.ts
@@ -2,30 +2,30 @@ import { gql } from 'apollo-boost'
 
 export default gql`
 {
-    holidays(where: [{path: "value", comparison: greaterThanOrEqual, value: ["2019-01-01"]}]) {
-      value
-      description
+  holidays(where: [{path: "value", comparison: greaterThanOrEqual, value: ["2019-01-01"]}]) {
+    value
+    name
+    description
+  }
+  leaveRequests {
+    from
+    to
+    owner
+  }
+  leaveRequestsConnection {
+    totalCount
+    pageInfo {
+      endCursor
+      hasNextPage
     }
-    leaveRequests {
+    items {
+      id
+      name
+      state
+      type
       from
       to
-      owner
-    }
-    leaveRequestsConnection {
-      totalCount
-      pageInfo {
-        endCursor
-        hasNextPage
-      }
-      items {
-        id
-        name
-        state
-        type
-        from
-        to
-      }
     }
   }
-  
+}
 `
