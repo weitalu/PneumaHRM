4686a61fb45bbc09ca9ef9fa4384b43e24970800
weitalu <weitalu@pneumasoftware.com>
2019-06-17 11:39:51 +0800

    add basic e2e tests

---
+++ PneumaHRM/Frontend/__test__/e2e/test.js
@@ -0,0 +1,32 @@
+require('chromedriver');
+const webdriver = require('selenium-webdriver');
+const { By, until } = webdriver;
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+const driver = new webdriver.Builder().forBrowser('chrome').build();
+const expect = chai.expect;
+
+describe('PneumasoftHRM End to End Test Suite', done => {
+    it('has dashboard', done => {
+        console.log('render dashboard');
+        driver.get('https://localhost:44364/dashboard')
+            .then(() => driver.wait(until.elementLocated({ tagName: "main" })))
+            .then(() => done());
+    });
+
+    it('has leave balance', done => {
+        console.log('render leave balance');
+        driver.get('https://localhost:44364/leavebalance')
+        .then(() => driver.wait(until.elementLocated({ tagName: "main" })))
+        .then(() => done());
+    })
+    it('has leave request', done => {
+        console.log('render leave request');
+        driver.get('https://localhost:44364/leaverequest')
+        .then(() => driver.wait(until.elementLocated({ tagName: "main" })))
+        .then(() => done());
+    })
+});
--- PneumaHRM/Frontend/util/loadingFallback.tsx
+++ PneumaHRM/Frontend/util/loadingFallback.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { QueryResult } from 'react-apollo';
 
 
-export default (Comp: (data: any, client: any) => JSX.Element) => ({ data, loading, client }) => loading ? <>Loading</> : Comp(data, client);
+export default (Comp: (data: any, client: any) => JSX.Element) => ({ data, loading, client }: QueryResult) => loading ? <>Loading</> : Comp(data, client);
